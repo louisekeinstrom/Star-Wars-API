4d1f16184f23f42b874f18fa37d1618f14f70ae8
Louise Keinström <louise.keinstrom@outlook.com>
2023-06-10 00:22:14 +0200

    added info to homepage

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -6,9 +6,25 @@ const HomePage = () => {
 		<>
 			<h1>Welcome young Skywalker!</h1>
 
-			<Link to="/search">
-				<Button variant="primary">Looking for something?</Button>
+			<Link to="/Films">
+				<div>Films</div>
 			</Link>
+			<Link to="/People">
+				<div>People</div>
+			</Link>
+			<Link to="/Planets">
+				<div>Planets</div>
+			</Link>
+			<Link to="/Species">
+				<div>Species</div>
+			</Link>
+			<Link to="/Starships">
+				<div>Starships</div>
+			</Link>
+			<Link to="/Vehicles">
+				<div>Vehicles</div>
+			</Link>
+
 		</>
 	)
 }
