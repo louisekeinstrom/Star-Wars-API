1d4fe2fb1556e9451444394e593349927eb93883
Louise Keinström <louise.keinstrom@outlook.com>
2023-06-13 16:43:52 +0200

    added type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -12,3 +12,21 @@ export type SearchResponse = {
     nbPages: number
     hitsPerPage: number
 }
+
+export type RenderPeople = {
+    id: number
+    name: string
+    birth_year: string
+    eye_color: string
+    hair_color: string
+    height: string
+    mass: string
+    skin_color: string
+    created: string
+    edited: string
+    films_count: number
+    species_count: number
+    starships_count: number
+    vehicles_count: number
+
+}
