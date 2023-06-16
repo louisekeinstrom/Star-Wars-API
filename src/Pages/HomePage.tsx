312d7bad60f4d387f6130845eb7e5064d8e1f873
Louise Keinström <louise.keinstrom@outlook.com>
2023-06-16 11:15:36 +0200

    another commit

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,6 +1,6 @@
+import { Link } from 'react-router-dom'
 import { useEffect, useRef, useState } from 'react'
 import Button from 'react-bootstrap/Button'
-import { Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import { search } from '../Services/StarWarsAPI'
 import { SearchResponse } from '../types'
